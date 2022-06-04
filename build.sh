424fd1a1f582da5f3416cbaecb3fecbdb32c0bd9
Vasco Sousa <myemailvasco@gmail.com>
2022-06-04 16:58:53 +0100

    Added frontend expose

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+docker build -t frontend:latest -f dockerfiles/frontend.dockerfile .
 docker build -t base:latest -f dockerfiles/base.dockerfile .
 docker build -t library:latest -f dockerfiles/library.dockerfile .
 docker build -t ocr:latest -f dockerfiles/ocr.dockerfile .
--- dockerfiles/frontend.dockerfile
+++ dockerfiles/frontend.dockerfile
@@ -1,7 +1,9 @@
 FROM node:alpine
+EXPOSE 3000
 COPY ./frontend /app
 RUN npm i
 CMD ["npm", "run", "start"]
 
 FROM nginx:alpine
+EXPOSE 80
 COPY ./nginx.conf /etc/nginx/conf.d/default.conf
